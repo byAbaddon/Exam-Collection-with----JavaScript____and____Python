ac7b69a9a358a15a4e09666faa951ffee5739081
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2021-06-18 03:12:28 +0300

    Add files via upload

--- JS Exams/03. Advanced Exam Collection/JS Advanced Retake - 08 April 2020/03. Bank/03. Bank.js
+++ JS Exams/03. Advanced Exam Collection/JS Advanced Retake - 08 April 2020/03. Bank/03. Bank.js
@@ -1,75 +1,67 @@
 class Bank {
-
-  constructor(bankName) {
-    this._bankName = bankName 
-    this.allCustomers = []
+  constructor(_bankName = String, allCustomers = []) {
+    this._bankName = _bankName
+    this.allCustomers = allCustomers
+    this.clientTransaction = {}
   }
 
-  newCustomer(customer) {
-    if (this.allCustomers.find(a => a.personalId === customer.personalId)) {
-      throw new Error(`${customer.firstName} ${customer.lastName} is already our customer!`)
-    }
-    customer['transactions'] = []
-    this.allCustomers.push(customer)
-    return customer
+  findClientByID(id) {
+    return this.allCustomers.find(x => x.personalId == id)
+  }
+
+
+  newCustomer(cst = { firstName, lastName, personalId }) {
+    if (this.findClientByID(cst.personalId))
+      throw new Error(`${cst.firstName} ${cst.lastName} is already our customer!`)
+    this.allCustomers.push(cst)
+    return cst
   }
 
   depositMoney(personalId, amount) {
-    let user = this.allCustomers.find(a => a.personalId === personalId)
-    if (!user) {
-      throw new Error('We have no customer with this ID!')
-    }
-    if (!user.hasOwnProperty('totalMoney')) {
-      user.totalMoney = amount
-    } else {
-      user.totalMoney += amount
-    }
-   
-    user.transactions.push(`${user.transactions.length + 1}. ${user.firstName} ${user.lastName} made deposit of ${amount}$!`)
-    return user.totalMoney + '$'
-
+    let client = this.findClientByID(personalId)
+    if (!client) throw new Error('We have no customer with this ID!')
+    client.totalMoney = (client.totalMoney || 0) + Number(amount)
+ 
+    this.clientTransaction[client.personalId] = this.clientTransaction[client.personalId] || []
+    this.clientTransaction[client.personalId].push(`${client.firstName} ${client.lastName} made deposit of ${amount}$!`)
+    
+    return `${client.totalMoney}$`
   }
 
   withdrawMoney(personalId, amount) {
-    if (!this.allCustomers.find(a => a.personalId === personalId)) {
-      throw new Error('We have no customer with this ID!')
-    }
+    let client = this.findClientByID(personalId)
+    if (!client) throw new Error('We have no customer with this ID!')
+    if (client.totalMoney < amount) throw  new Error(`${client.firstName} ${client.lastName} does not have enough money to withdraw that amount!`)
+    client.totalMoney -= amount
 
-    let user = this.allCustomers.find(a => a.personalId === personalId)
-
-    if (user.totalMoney < amount) {
-      throw new Error(`${user.firstName} ${user.lastName} does not have enough money to withdraw that amount!`)
-    } else {   
-      user.transactions.push(`${user.transactions.length + 1}. ${user.firstName} ${user.lastName} withdrew ${amount}$!`)
-      user.totalMoney -= amount
-      return user.totalMoney + '$'
-    }
+    this.clientTransaction[client.personalId] = this.clientTransaction[client.personalId] || []
+    this.clientTransaction[client.personalId].push(`${client.firstName} ${client.lastName} withdrew ${amount}$!`)
+    return `${client.totalMoney}$`
+    
   }
 
-  customerInfo(personalId) { 
-    let customer = this.allCustomers.find(a => a.personalId === personalId)
-    if (!customer) {
-      throw new Error('We have no customer with this ID!')
-    }
-
-    let strInfo = `Bank name: ${this._bankName}\nCustomer name: ${customer.firstName} ${customer.lastName}\nCustomer ID: ${customer.personalId}\nTotal Money: ${customer.totalMoney}$`
-    let sort = customer.transactions.sort((b, a) => Number(a.slice(0,1)) - Number(b.slice(0,1)))  
-    strInfo += `\nTransactions:\n${sort.join('\n')}`
-      
-    return strInfo.trim()
+  customerInfo(personalId) {
+    let client = this.findClientByID(personalId)
+    if (!client) throw new Error('We have no customer with this ID!')
+    return `Bank name: ${this._bankName}
+Customer name: ${client.firstName} ${client.lastName}
+Customer ID: ${client.personalId}
+Total Money: ${client.totalMoney}$
+Transactions:\n${this.clientTransaction[client.personalId].reverse()
+      .map((x, i, ar) => `${ar.length - i}. ${x}`)
+      .join('\n')}`
   }
+  
 }
 
 
-
-// let bank = new Bank('SoftUni Bank');
+// let bank = new Bank('SoftUni Bank')
 
 // console.log(bank.newCustomer({firstName: 'Svetlin', lastName: 'Nakov', personalId: 6233267}));
-// console.log(bank.newCustomer({firstName: 'Mihaela', lastName: 'Mileva', personalId: 4151596}));
-
+// console.log(bank.newCustomer({ firstName: 'Mihaela', lastName: 'Mileva', personalId: 4151596 }));
 // bank.depositMoney(6233267, 250);
 // console.log(bank.depositMoney(6233267, 250));
-// bank.depositMoney(4151596,555);
+// bank.depositMoney(4151596, 555);
 
 // console.log(bank.withdrawMoney(6233267, 125));
 
@@ -77,13 +69,11 @@ class Bank {
 
 
 
-//////////////// output: //////////////
-// { firstName: ‘Svetlin’, lastName: ‘Nakov’, personalId: 6233267 } 
-// { firstName: ‘Mihaela’, lastName: ‘Mileva’, personalId: 4151596 }
-
+// Corresponding output
+// { firstName: ‘Svetlin', lastName: ‘Nakov', personalId: 6233267 }
+// { firstName: ‘Mihaela', lastName: ‘Mileva', personalId: 4151596 }
 // 500$
 // 375$
-
 // Bank name: SoftUni Bank
 // Customer name: Svetlin Nakov
 // Customer ID: 6233267
@@ -92,4 +82,5 @@ class Bank {
 // 3. Svetlin Nakov withdrew 125$!
 // 2. Svetlin Nakov made depostit of 250$!
 // 1. Svetlin Nakov made depostit of 250$!
+
 
