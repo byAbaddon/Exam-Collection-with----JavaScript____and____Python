6bc73959f686495be220ec6b16e0f60acd6a49c8
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2022-01-13 19:24:45 +0200

    Add files via upload

---
+++ JS Exams/01.  Basics Exam Collection/Basics Online Exam -  28 and 29 March 2020/01. Birthday Party/01. Birthday Party.js
@@ -0,0 +1,11 @@
+function birthdayParty(rent) {
+  rent =  Number(rent) 
+  const cake = rent * 0.2
+  const drinks = cake * 0.55
+  const animator = rent / 3
+  const total = cake + drinks + animator + rent
+  return total
+}
+
+// console.log(birthdayParty(2250))  //3697.5
+// console.log(birthdayParty(3720)) //6113.2
