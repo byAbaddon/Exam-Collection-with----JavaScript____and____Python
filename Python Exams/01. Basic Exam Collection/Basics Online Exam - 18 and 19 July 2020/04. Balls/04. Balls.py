5aa0e6703ae4c572c4362719f579fd55538bc837
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-02-23 12:38:40 +0200

    Update 04. Balls.py

--- Python Exams/01. Basic Exam Collection/Basics Online Exam - 18 and 19 July 2020/04. Balls/04. Balls.py
+++ Python Exams/01. Basic Exam Collection/Basics Online Exam - 18 and 19 July 2020/04. Balls/04. Balls.py
@@ -1,3 +1,26 @@
+balls_points = {'red': 5, 'orange': 10, 'yellow': 15, 'white': 20, 'black': 0}
+balls_quantities = {'Total points': 0, 'red': 0, 'orange': 0, 'yellow': 0, 'white': 0, 'Other colors picked': 0,'Divides from black balls': 0}
+
+for _ in range(int(input())):
+    ball = input()
+
+    if ball == 'black':
+        balls_quantities['Divides from black balls'] += 1
+        balls_quantities['Total points'] /= 2
+        continue
+    if ball in balls_points:
+        balls_quantities[ball] += 1
+        balls_quantities['Total points'] += balls_points[ball]
+    else:
+        balls_quantities['Other colors picked'] += 1
+
+for k, v in balls_quantities.items():
+    print(f'{k}: {int(v)}' if not k.find('T') else f'{k.title()} balls: {v}' if len(k) < 10 else f'{k}: {v}')
+
+
+# ------------------------------------------------------------------------------    
+    
+    
 from math import floor
 
 balls_count = int(input())
