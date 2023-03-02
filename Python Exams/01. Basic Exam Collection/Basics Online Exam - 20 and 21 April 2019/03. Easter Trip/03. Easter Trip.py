1954aa47606b153a7c87c6dda109e6022cbffefd
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-03-02 22:39:51 +0200

    Update 03. Easter Trip.py

--- Python Exams/01. Basic Exam Collection/Basics Online Exam - 20 and 21 April 2019/03. Easter Trip/03. Easter Trip.py
+++ Python Exams/01. Basic Exam Collection/Basics Online Exam - 20 and 21 April 2019/03. Easter Trip/03. Easter Trip.py
@@ -1,3 +1,13 @@
+destination, date, nights = [input() for _ in range(3)]
+
+price_dict = {
+    'France': {'21-23': 30, '24-27': 35, '28-31': 40},
+    'Italy': {'21-23': 28, '24-27': 32, '28-31': 39},
+    'Germany': {'21-23': 32, '24-27': 37, '28-31': 43},
+}
+
+print(f'Easter trip to {destination} : {price_dict[destination][date] * int(nights):.2f} leva.')
+# ------------------------------------------(2)--------------
 destination = input()
 data = input()
 nights = int(input())
