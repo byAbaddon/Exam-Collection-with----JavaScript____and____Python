6028c7e9035a64bc1d744b7ec172329e91ee2b6d
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-06-06 02:42:39 +0300

    Update 4. Renovation.js

--- JS Exams/01.  Basics Exam Collection/Basics Online Exam - 6 and 7 July 2019/4. Renovation/4. Renovation.js
+++ JS Exams/01.  Basics Exam Collection/Basics Online Exam - 6 and 7 July 2019/4. Renovation/4. Renovation.js
@@ -1,11 +1,11 @@
 function renovation(arg) {
-  let [hight, weight, percent] = [arg[0], arg[1], arg[2]]
+  let [hight, weight, percent] = [...arg]
   let qm = (hight * weight * 4) * (100 - percent) / 100
   let num = 3
   while (arg[num]) {
 
-    if (arg[num] === 'Tired!') {
-      console.log(`${Math.ceil(qm)} quadratic m left.`);
+    if (arg[num] == 'Tired!') {
+      console.log(`${Math.ceil(qm)} quadratic m left.`)
       return
     }
     qm -= arg[num]
@@ -13,9 +13,9 @@ function renovation(arg) {
   }
 
   if (qm >= 0) {
-    console.log(`All walls are painted! Great job, Pesho!`);
+    console.log(`All walls are painted! Great job, Pesho!`)
   } else {
-    console.log(`All walls are painted and you have ${Math.abs(Math.ceil(qm))} l paint left!`);
+    console.log(`All walls are painted and you have ${Math.abs(Math.ceil(qm))} l paint left!`)
   }
 }
 
