7f2f08914a02865e541b3f379f33317736cb580a
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-06-18 02:36:18 +0300

    Update 03. Man O War.js

--- JS Exams/02. Fundamentals Exam Collection/Fundamentals Mid Exam Retake - 6 August 2019/03. Man O War/03. Man O War.js
+++ JS Exams/02. Fundamentals Exam Collection/Fundamentals Mid Exam Retake - 6 August 2019/03. Man O War/03. Man O War.js
@@ -1,4 +1,39 @@
 function manOWar(input) {
+  const getShipStatus = ship => ship.reduce((a, b) => a + b)
+
+  let pirateShip = input.shift().split('>').map(Number)
+  let warShip = input.shift().split('>').map(Number)
+  let healthStatus = +input.shift()
+
+  while (input.length) {
+    let [command, index, lastIndex, damage] = input.shift().split(' ').map(el => isNaN(el) ? el : Number(el))
+
+    if (command == 'Fire' && warShip[index]) {
+      warShip[index] -= lastIndex
+      if (warShip[index] <= 0) return 'You won! The enemy ship has sunken.'
+    }
+
+    if (command == 'Defend' && pirateShip[index] && pirateShip[lastIndex]) {
+      pirateShip = pirateShip.map((el, i) => (i >= index && i <= lastIndex) ? el - damage : el)
+      if (pirateShip.some(el => el <= 0)) return 'You lost! The pirate ship has sunken.'
+    }
+
+    if (command == 'Repair' && pirateShip[index]) {
+      pirateShip[index] = Math.min(pirateShip[index] + lastIndex, healthStatus);
+    }
+
+    if (command == 'Status') {
+      let status = pirateShip.filter(el => el < healthStatus * 0.20).length
+      console.log(`${status} sections need repair.`)
+    }
+  }
+
+  return `Pirate ship status: ${getShipStatus(pirateShip)}\nWarship status: ${getShipStatus(warShip)}`
+}
+
+//---------------------------------------(2)-------------------------------------------
+
+function manOWar(input) {
   input.pop()
   let pirateShip = input.shift().split('>').map(Number)
   let warShip = input.shift().split('>').map(Number)
