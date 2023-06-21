1ca21a77fa1a34fb1e864d006117e9f7189e2fec
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-06-21 20:04:51 +0300

    Update 03. Heart Delivery.js

--- JS Exams/02. Fundamentals Exam Collection/Fundamentals Mid Exam 04/03. Heart Delivery/03. Heart Delivery.js
+++ JS Exams/02. Fundamentals Exam Collection/Fundamentals Mid Exam 04/03. Heart Delivery/03. Heart Delivery.js
@@ -6,19 +6,19 @@ function heartDelivery(arr) {
     token = arr.shift().split(' ')
     if (token.length == 1) break
 
-    house += +token.slice(-1)
+    house += +token.slice(-1);
     house > neighborhood.length - 1 ? house = 0 : null
     neighborhood[house] -= 2
 
-    if (neighborhood[house] === 0)
+    if (neighborhood[house] == 0)
       console.log(`Place ${house} has Valentine's day.`)
     else if (neighborhood[house] < 0)
       console.log(`Place ${house} already had Valentine's day.`)
   }
 
   console.log(`Cupid's last position was ${house}.`)
-  let failedHouses = neighborhood.filter(el => el > 0).length
-  console.log(failedHouses <= 0 ? 'Mission was successful.' : `Cupid has failed ${failedHouses} places.`);
+  let failedHouses = neighborhood.filter((el) => el > 0).length
+  console.log(failedHouses <= 0 ? 'Mission was successful.' : `Cupid has failed ${failedHouses} places.`)
 }
 
 
