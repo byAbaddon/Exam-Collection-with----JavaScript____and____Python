bea95ad5a739ffaefd79f64856124593d7375851
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-07-14 23:36:28 +0300

    Update 01. World Tour.js

--- JS Exams/02. Fundamentals Exam Collection/Fundamentals Final Exam 02/01. World Tour/01. World Tour.js
+++ JS Exams/02. Fundamentals Exam Collection/Fundamentals Final Exam 02/01. World Tour/01. World Tour.js
@@ -1,36 +1,32 @@
 function worldTour(arr) {
   arr.pop()
   let destination = arr.shift()
-
-  while (arr.length > 0) {
+ 
+  while (arr.length) {
     const [command, arg1, arg2] = arr.shift().split(':').map(el => isNaN(el) ? el : Number(el))
-
+ 
     if (command === 'Add Stop') {
-      let index = arg1
-      let country = arg2
+      let [index, country] = [arg1 ,arg2]
       if (index >= 0 && index < destination.length)
         destination = destination.slice(0, index) + country + destination.slice(index)
     }
-
-    if (command === 'Remove Stop') {
-      let start = arg1
-      let end = arg2
+ 
+    if (command == 'Remove Stop') {
+      let [start, end] = [arg1, arg2]
       if (start >= 0 && end < destination.length) {
         let cutPart = destination.slice(start, end + 1)
         destination = destination.replace(cutPart, '')
       }
     }
-
+ 
     if (command == 'Switch') {
-      let oldString = arg1
-      let newString = arg2
+      let [oldString, newString] = [arg1, arg2]
       if (destination.includes(oldString))
         destination = destination.replace(oldString, newString)
     }
-
     console.log(destination)
   }
-
+ 
   return `Ready for world tour! Planned stops: ${destination}`
 }
 
