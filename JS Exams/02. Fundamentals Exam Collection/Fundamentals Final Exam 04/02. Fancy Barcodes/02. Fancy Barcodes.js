a4beb90f97b12685c2f27fc7d6815dc827cf05cb
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-07-20 23:33:21 +0300

    Update 02. Fancy Barcodes.js

--- JS Exams/02. Fundamentals Exam Collection/Fundamentals Final Exam 04/02. Fancy Barcodes/02. Fancy Barcodes.js
+++ JS Exams/02. Fundamentals Exam Collection/Fundamentals Final Exam 04/02. Fancy Barcodes/02. Fancy Barcodes.js
@@ -3,6 +3,26 @@ function fancyBarcodes(arr) {
   let regex = /@#+([A-Z][A-Za-z0-9]{4,}[A-Z])@#+/gm
 
   while (loop--) {
+    let code =arr.shift().match(regex)
+    if (code) {
+      let digits = code[0].match(/\d+/gm)
+      console.log(`Product group: ${!digits ? '00' : digits.join('')}`)
+      continue
+    } 
+    console.log('Invalid barcode')
+  }
+}
+
+//----------------------------------------------------(2)
+
+
+
+
+function fancyBarcodes(arr) {
+  let loop = +arr.shift()
+  let regex = /@#+([A-Z][A-Za-z0-9]{4,}[A-Z])@#+/gm
+
+  while (loop--) {
     let currentCode = arr.shift()
     let code = currentCode.match(regex)
     if (code != null) {
