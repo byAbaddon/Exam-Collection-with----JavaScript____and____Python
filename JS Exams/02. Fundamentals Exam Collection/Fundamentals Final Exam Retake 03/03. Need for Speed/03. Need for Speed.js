c2ee106e280fc260af6c19cc2c6255a03065b66f
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-07-25 14:42:57 +0300

    Update 03. Need for Speed.js

--- JS Exams/02. Fundamentals Exam Collection/Fundamentals Final Exam Retake 03/03. Need for Speed/03. Need for Speed.js
+++ JS Exams/02. Fundamentals Exam Collection/Fundamentals Final Exam Retake 03/03. Need for Speed/03. Need for Speed.js
@@ -1,3 +1,5 @@
+//-----------------------------With sort condition
+
 function needForSpeed(arr) {
   arr.pop()
   let numberOfCars = +arr.shift()
@@ -49,6 +51,68 @@ function needForSpeed(arr) {
   }
 
   let sortCars = Object.entries(carsObj).sort((a, b) => b[1].km - a[1].km || a[0].localeCompare(b[0]))
+
+  for (const key of sortCars)
+    console.log(`${key[0]} -> Mileage: ${key[1].km} kms, Fuel in the tank: ${key[1].fuel} lt.`)
+}
+
+
+
+
+
+//-----------------------------------------------------without sort condition
+
+function needForSpeed(arr) {
+  arr.pop()
+  let numberOfCars = +arr.shift()
+  carsObj = {}
+
+  while (numberOfCars--) {
+    const [car, km, fuel] = arr.shift().split('|').map(el => isNaN(el) ? el : Number(el))
+    carsObj[car] = {km, fuel}
+  }
+
+  while (arr.length > 0) {
+    let [command, car, arg1, arg2] = arr.shift().split(' : ').map(el => isNaN(el) ? el : Number(el))
+    
+    if (command == 'Drive') {
+      let distance = arg1
+      let fuel = arg2
+
+      if (carsObj[car].fuel < fuel) {
+        console.log('Not enough fuel to make that ride')
+      } else {
+        carsObj[car].km += distance
+        carsObj[car].fuel -= fuel
+        console.log(`${car} driven for ${distance} kilometers. ${fuel} liters of fuel consumed.`)
+      }
+
+      if (carsObj[car].km >= 100000) {
+        console.log(`Time to sell the ${car}!`)
+        delete carsObj[car]
+      }
+    }
+
+    if (command == 'Refuel') {
+      let fuel = arg1
+      if (carsObj[car].fuel + fuel > 75) {
+        fuel = 75 - carsObj[car].fuel
+        carsObj[car].fuel = 75
+      } else {
+        carsObj[car].fuel += fuel
+      }
+
+      console.log(`${car} refueled with ${fuel} liters`)
+    }
+
+    if (command == 'Revert') {
+      let km = arg1
+      carsObj[car].km -= km
+      carsObj[car].km < 10000 ? carsObj[car].km = 10000 : console.log(`${car} mileage decreased by ${km} kilometers`)
+    }
+  }
+
+  let sortCars = Array.from(Object.entries(carsObj))
 
   for (const key of sortCars)
     console.log(`${key[0]} -> Mileage: ${key[1].km} kms, Fuel in the tank: ${key[1].fuel} lt.`)
