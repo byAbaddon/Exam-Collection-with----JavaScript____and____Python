e2dba1564f2f763ede311ad3ae5737db933c89a7
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-08-19 13:20:12 +0300

    Add files via upload

---
+++ JS Exams/01.  Basics Exam Collection/Basics Online - Retake Exam - August 19 2023/03. Cat Life/03. Cat Life.js
@@ -0,0 +1,13 @@
+function catLife([breed, gender]) {
+  const catData = {"British Shorthair": { m: 13, f: 14  },"Siamese": {m: 15, f: 16  }, "Persian": { m: 14, f: 15  }, "Ragdoll": {m: 16, f: 17  }, "American Shorthair": {m: 12, f: 13  }, "Siberian": {m: 11, f: 12}}
+  try {
+     return catData[breed][gender] * 12 / 6 + ' cat months'
+  } catch {
+    return breed +' is invalid cat!'
+  }
+ 
+}
+
+console.log(catLife((["Persian","m"])))
+console.log(catLife((["Siamese","f"])))
+console.log(catLife((["S","f"])))
